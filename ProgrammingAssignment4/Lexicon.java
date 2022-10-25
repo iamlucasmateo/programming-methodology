8bb4718e6218693db77e352ebb0c0ec616bfb4d7
iamlucasmateo <perrone.lucasmateo@gmail.com>
2022-10-25 13:15:46 -0300

    added lexicon reader

--- ProgrammingAssignment4/Lexicon.java
+++ ProgrammingAssignment4/Lexicon.java
@@ -4,10 +4,12 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.HashMap;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Lexicon {
     public static HashMap<Integer, String> Lexicon = buildLexicon();
-    public static final String LEXICON_FILE = "./ProgrammingAssignment4/lexicon.txt";
+    public static final String LEXICON_FILE = "ProgrammingAssignment4/lexicon.txt";
     
     public static void main(String[] args) {
         System.out.println(System.getProperty("user.dir"));
@@ -15,16 +17,35 @@ public class Lexicon {
         for (String fileName: dir.list()) {
             System.out.println(fileName);
         }
+        HashMap<Integer, String> lexicon = buildLexicon();
+        System.out.println(lexicon);
     }
 
     public static HashMap<Integer, String> buildLexicon() {
         HashMap<Integer, String> lexicon = new HashMap<Integer, String>();
-        BufferedReader reader = new BufferedReader(new FileReader("ProgrammingAssignment4/lexicon.txt"));
-        // reader.close();
-        // lexicon.put(0, "Lexicographic");
-        // lexicon.put(1, "Enciclopedia");
-        // lexicon.put(2, "Bibliography");
-        // lexicon.put(3, "Psychopathology");
+        FileReader fr;
+        try {
+            fr = new FileReader(LEXICON_FILE);
+        } catch (FileNotFoundException exc) {
+            System.out.println(exc);
+            return lexicon;
+        }
+        BufferedReader reader = new BufferedReader(fr);
+        int i = 0;
+        while (true) {
+            try {
+                String line = reader.readLine();
+                if (line == null) break;
+                lexicon.put(i, line);
+                i++;
+            } catch (IOException exc) {};
+        }
+        
+        try {
+            reader.close();
+        } catch (IOException exc) {
+            return lexicon;
+        }
         
         return lexicon;
     }
